1dba682af82de2451c4baa876c6ee04b4c7c9fdf
Vy Nguyen <vynguyen0520@gmail.com>
2022-07-18 18:00:06 -0700

    init commit

---
+++ Develop/server.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const session = require('express-session');
+const routes = require('./controllers');
+const path = require('path');
+
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({});
+
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
+app.use(session(sess));
+
+// Define template engine to use
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+// this allows you to use a static folder (unchanged files)
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(routes);
+
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => {
+    console.log(`App listening on port http://localhost:${PORT} 🌎`);
+  });
+});
